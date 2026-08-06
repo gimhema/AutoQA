8d78b5c6c8cf1b7634b4ede0133ba26a17f2ca85
kimmo <hemakim0215@naver.com>
2026-08-06 11:25:46 +0900

    .

--- Games/Minion/src/config.rs
+++ Games/Minion/src/config.rs
@@ -1,10 +1,7 @@
 
 
-const TILE_LIMIT_X: i32 = 1000;
-const TILE_LIMIT_Y: i32 = 1000;
-
-// pub mod limits {
-//     pub const MAX_LEVEL: u32 = 100;
-//     pub const MAX_HEALTH: f32 = 1000.0;
-//     pub const MAX_MANA: f32 = 500.0;
-// }
+pub mod limits {
+    pub const TILE_LIMIT_X: i32 = 1000;
+    pub const TILE_LIMIT_Y: i32 = 1000;
+    pub const MAX_HEALTH: i32 = 1000;
+}
