c73c57207babebce4088cde5ef703b062f3b59dd
kimmo <hemakim0215@naver.com>
2026-08-06 11:27:41 +0900

    .

---
+++ Games/Minion/src/perks.rs
@@ -0,0 +1,19 @@
+
+
+enum PERKS_TYPE
+{
+    _DEFAULT = 0,
+    _INC_ATTACK = 1,
+    _INC_HEALTH = 2,
+    _INC_SPEED = 3,
+    _INC_DEFENSE = 4,
+    _INC_BULLET_NUM = 5,
+    _MAX
+}
+
+
+
+struct PerskBasicInfo
+{
+
+}
