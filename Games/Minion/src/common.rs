bc1f72512a0df348639b379c7b5df3e1c0e6d2f5
kimmo <hemakim0215@naver.com>
2026-08-07 12:37:57 +0900

    .

--- Games/Minion/src/common.rs
+++ Games/Minion/src/common.rs
@@ -11,7 +11,9 @@ pub struct CommonStatus
 {
     pub health : i32,
     pub name : String,
-    pub speed : i32
+    pub speed : i32,
+    pub power : i32,
+    pub defense : i32
 }
 
 pub struct Actor
@@ -26,6 +28,8 @@ impl CommonStatus
         self.health = 0;
         self.name = String::new();
         self.speed = 0;
+        self.power = 0;
+        self.defense = 0;
     }
 }
 
@@ -34,6 +38,11 @@ impl Geometry
     fn  Init(&mut self) {
         self.x = 0;
         self.y = 0;
+    }
+
+    fn Update(&mut self, x : i32, y : i32) {
+        self.x = x;
+        self.y = y;
     }
 }
 
