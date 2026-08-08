62bb96e50b926d5f1b9be6fb79218a0d04969c7b
kimmo <hemakim0215@naver.com>
2026-08-08 10:26:41 +0900

    .

--- Games/Minion/src/common_logic.rs
+++ Games/Minion/src/common_logic.rs
@@ -1,10 +1,25 @@
 use crate::common;
 use crate::common_logic;
 
+pub mod CommonLogicUtil
+{
+    use crate::{common::Geometry, minion::Minion};
+
+    pub fn IsExistMinion(target_pos : Geometry) -> Option<&mut Minion> {
+
+        return None
+    }
+}
 
 pub mod DamageInterface
 {
-    pub fn ApplyDamage() {
-        
+    use crate::{common::Geometry, minion::Minion};
+
+    pub fn ApplyDamage(attack: &Minion, target: &mut Minion) {
+//        target.actorInfo.status.health -= attack.actorInfo.status.power;
+    }
+
+    pub fn ApplyRangeDamage(attack : &Minion, target_pos : Geometry, range : i32) {
+
     }
 }
