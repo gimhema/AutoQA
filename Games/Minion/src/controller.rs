feff99ce4debaf6ef9d3246e4d53ca6979936723
kimmo <hemakim0215@naver.com>
2026-08-08 10:41:25 +0900

    .

--- Games/Minion/src/controller.rs
+++ Games/Minion/src/controller.rs
@@ -1,13 +1,38 @@
 use crate::minion;
+use crate::common::Geometry;
+use macroquad::prelude::*;
 
 pub struct PlayerController
 {
-
+    pub pos : Geometry,
+    pub aim_angle : f32,
+    pub speed : i32
 }
 
 impl PlayerController
 {
-    fn Init(&self) {
+    pub fn New() -> Self {
+        PlayerController {
+            pos : Geometry { x: 0, y: 0 },
+            aim_angle : 0.0,
+            speed : 5
+        }
+    }
 
+    pub fn Init(&mut self) {
+        self.pos = Geometry { x: 400, y: 300 };
+        self.aim_angle = 0.0;
+    }
+
+    pub fn Update(&mut self) {
+        if is_key_down(KeyCode::W) { self.pos.y -= self.speed; }
+        if is_key_down(KeyCode::S) { self.pos.y += self.speed; }
+        if is_key_down(KeyCode::A) { self.pos.x -= self.speed; }
+        if is_key_down(KeyCode::D) { self.pos.x += self.speed; }
+
+        let (mouse_x, mouse_y) = mouse_position();
+        let dx = mouse_x - self.pos.x as f32;
+        let dy = mouse_y - self.pos.y as f32;
+        self.aim_angle = dy.atan2(dx);
     }
 }
--- Games/Minion/src/main.rs
+++ Games/Minion/src/main.rs
@@ -6,33 +6,25 @@ mod minion;
 mod config;
 mod perks;
 
-use common::Geometry;
+use controller::PlayerController;
 use macroquad::prelude::*;
 
 #[macroquad::main("Minion")]
 async fn main() {
-    let mut player_pos = Geometry { x: 400, y: 300 };
-    let speed = 5;
+    let mut player = PlayerController::New();
+    player.Init();
 
     loop {
         clear_background(BLACK);
 
-        if is_key_down(KeyCode::W) { player_pos.y -= speed; }
-        if is_key_down(KeyCode::S) { player_pos.y += speed; }
-        if is_key_down(KeyCode::A) { player_pos.x -= speed; }
-        if is_key_down(KeyCode::D) { player_pos.x += speed; }
+        player.Update();
 
-        let (mouse_x, mouse_y) = mouse_position();
-        let dx = mouse_x - player_pos.x as f32;
-        let dy = mouse_y - player_pos.y as f32;
-        let aim_angle = dy.atan2(dx);
-
-        draw_circle(player_pos.x as f32, player_pos.y as f32, 15.0, YELLOW);
+        draw_circle(player.pos.x as f32, player.pos.y as f32, 15.0, YELLOW);
 
         let aim_len = 40.0;
-        let aim_end_x = player_pos.x as f32 + aim_angle.cos() * aim_len;
-        let aim_end_y = player_pos.y as f32 + aim_angle.sin() * aim_len;
-        draw_line(player_pos.x as f32, player_pos.y as f32, aim_end_x, aim_end_y, 3.0, RED);
+        let aim_end_x = player.pos.x as f32 + player.aim_angle.cos() * aim_len;
+        let aim_end_y = player.pos.y as f32 + player.aim_angle.sin() * aim_len;
+        draw_line(player.pos.x as f32, player.pos.y as f32, aim_end_x, aim_end_y, 3.0, RED);
 
         next_frame().await;
     }
