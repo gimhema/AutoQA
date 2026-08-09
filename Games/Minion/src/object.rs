458b0169c43c65b50b6adb6f2181a9b9e95a1311
kimmo <hemakim0215@naver.com>
2026-08-09 12:50:25 +0900

    .

--- Games/Minion/src/object.rs
+++ Games/Minion/src/object.rs
@@ -2,7 +2,8 @@
 
 pub mod EOBJECT
 {
-    enum OTYPE {
+    #[derive(Clone, Copy)]
+    pub enum OTYPE {
         DEFAULT = -1,
         BLOCK = 0,
         BULLET = 1        
@@ -16,7 +17,27 @@ pub trait ObjectBehavior
     fn OnHit(&mut self);
 }
 
+#[derive(Clone, Copy)]
 struct ObjectInfo
 {
+    pub oID : i32,
+    pub oType : EOBJECT::OTYPE
+}
 
+#[derive(Clone, Copy)]
+pub struct BlockUnbreakable
+{
+    pub info : ObjectInfo
+}
+
+#[derive(Clone, Copy)]
+pub struct BlockBreakable
+{
+    pub info : ObjectInfo
+}
+
+#[derive(Clone, Copy)]
+pub struct Bullet
+{
+     pub info : ObjectInfo   
 }
