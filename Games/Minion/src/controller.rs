f2b62016f5caebd7b476f834aaf877540ae1ca24
kimmo <hemakim0215@naver.com>
2026-08-09 12:10:06 +0900

    .

--- Games/Minion/src/controller.rs
+++ Games/Minion/src/controller.rs
@@ -1,4 +1,5 @@
 use crate::world::World;
+use crate::minion::Minion;
 use macroquad::prelude::*;
 
 pub struct PlayerController
@@ -27,9 +28,12 @@ impl PlayerController
         }
     }
 
+    pub fn GetPawn<'a>(&self, world : &'a mut World) -> Option<&'a mut Minion> {
+        world.GetMinionMut(self.possessed_id?)
+    }
+
     pub fn Update(&mut self, world : &mut World) {
-        let Some(id) = self.possessed_id else { return; };
-        let Some(minion) = world.GetMinionMut(id) else { return; };
+        let Some(minion) = self.GetPawn(world) else { return; };
 
         if is_key_down(KeyCode::W) { minion.actorInfo.geometry.y -= self.speed; }
         if is_key_down(KeyCode::S) { minion.actorInfo.geometry.y += self.speed; }
