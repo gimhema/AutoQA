2f958d8153c58314a86a38f46812f42ed64812ba
kimmo <hemakim0215@naver.com>
2026-08-17 11:22:38 +0900

    .

--- Games/Minion/src/config.rs
+++ Games/Minion/src/config.rs
@@ -4,4 +4,5 @@ pub mod CONSTANTS {
     pub const TILE_LIMIT_X: i32 = 1000;
     pub const TILE_LIMIT_Y: i32 = 1000;
     pub const MAX_HEALTH: i32 = 1000;
+    pub const MAX_ENEMY_COUNT : i32 = 30;
 }
